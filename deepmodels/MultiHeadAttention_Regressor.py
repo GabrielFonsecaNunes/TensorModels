6f24002edf89a4a680d3a203aa7eb8f8473d20b7
GabrielFonsecaNunes <fonseca.nunes1996@gmail.com>
2024-12-20 00:40:04 -0300

    Version 0.0.1 - DeepModels, fix import libs

--- deepmodels/MultiHeadAttention_Regressor.py
+++ deepmodels/MultiHeadAttention_Regressor.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import (
-    Sequential,
     MultiHeadAttention, 
     Dense, 
     Dropout, 
